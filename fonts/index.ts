6b581f5c7ab1b323928c4f224f1563f2630410cb
cipherprofessor <mohsinmanzoor1913@gmail.com>
2026-06-14 21:16:48 +0530

    fix font case: rename .OTF to .otf for Turbopack Linux compatibility

--- fonts/index.ts
+++ fonts/index.ts
@@ -12,22 +12,22 @@ export const bricolageGrotesque = Bricolage_Grotesque({
 export const sfPro = localFont({
   src: [
     {
-      path: "./locals/sf-pro-display/SFPRODISPLAYREGULAR.OTF",
+      path: "./locals/sf-pro-display/SFPRODISPLAYREGULAR.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./locals/sf-pro-display/SFPRODISPLAYMEDIUM.OTF",
+      path: "./locals/sf-pro-display/SFPRODISPLAYMEDIUM.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./locals/sf-pro-display/SFPRODISPLAYMEDIUM.OTF",
+      path: "./locals/sf-pro-display/SFPRODISPLAYMEDIUM.otf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./locals/sf-pro-display/SFPRODISPLAYBOLD.OTF",
+      path: "./locals/sf-pro-display/SFPRODISPLAYBOLD.otf",
       weight: "700",
       style: "normal",
     },
